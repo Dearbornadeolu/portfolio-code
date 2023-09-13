de61dfd5d405a7a5178e603c969f320cd87ad26d
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-09-13 18:27:08 +0100

    ui update

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
           About Me
         </h1>
         <p className='text-[1rem] md:text-[1.5rem] font-thin text-center text-amber-100 mb-[3rem]'>
-          I`&apos;`m a passionate Software Engineer with a focus on crafting remarkable web experiences, creating innovative Chrome extensions, and exploring the exciting world of cybersecurity. I have worked on more than 60 Projects in the period of 1year and few months for the development progress. Over the last 6 months, i have worked for with 10 clients on multiple projects which includes companies and startups.
+          I&apos;m a passionate Software Engineer with a focus on crafting remarkable web experiences, creating innovative Chrome extensions, and exploring the exciting world of cybersecurity. I have worked on more than 60 Projects in the period of 1year and few months for the development progress. Over the last 6 months, i have worked for with 10 clients on multiple projects which includes companies and startups.
         </p>
         <div className='flex justify-center items-center align-middle w-[90%] m-auto md:w-[80vw]  md:gap-[10rem]'>
           <div className='flex md:w-[70px] items-center gap-2'><h1 className='text-amber-500 text-[40px]  md:text-[5rem] font-black'>2</h1> <span className='text-[20px] text-amber-700'>Years of Experience</span></div>
