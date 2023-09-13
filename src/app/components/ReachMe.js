485a83d7d3ffa188884de222f13607493f997fba
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-09-13 18:21:57 +0100

    hyphen

--- src/app/components/ReachMe.js
+++ src/app/components/ReachMe.js
@@ -3,7 +3,7 @@
 const ReachMe = () => {
   return (
     <div className='p-10'>
-        <h1 className='font-bold text-[2rem] mt-[100px] md:mt-[200px] text-center'>Let's make something amazing</h1>
+        <h1 className='font-bold text-[2rem] mt-[100px] md:mt-[200px] text-center'>Let`&apos;`s make something amazing</h1>
         <h1 className='text-center mt-4'>Write to me</h1>
         <div className='mt-8 border-[2px] border-black md:w-3/4 m-auto flex justify-between p-3 mb-5'>
           <input type='text' className='w-2/3 focus:border-none outline-none'/>
